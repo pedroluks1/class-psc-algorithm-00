881310a6f276a83bfadfe483630610b913e70aac
Pedro Lucas <xpedro321@gmail.com>
2025-09-16 19:48:43 -0300

    Add files via upload

---
+++ encontre-se.java
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main {
+  public static void main(String [] args) {
+      String saladesejada = "a219"
+      String salainformada = ""
+      Scanner scanner = new Scanner (System.in);
+      
+      System.out.println(x:"Digite o número da sala:");
+      salainformada = scanner.next();
+      
+      if (saladesejada=salainformada) {
+         System.out.println(x:"Você encontrou sua sala!");
+      } else {
+        System.out.println(x:"Você está perdido!\nSe encontre!");
+    }
+    scanner.close();
+    }
+    
+    
+}
